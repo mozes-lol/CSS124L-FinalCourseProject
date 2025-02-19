130ff6e79f6f70e1b4f5cc42d9f6602ac8641542
mozes-lol <johnmarkgarcia164@gmail.com>
2025-02-19 20:56:26 +0800

    Import project

---
+++ CSS124LFinalCourseProject/src/main/java/com/garciajohnmark/css124lfinalcourseproject/CSS124LFinalCourseProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.garciajohnmark.css124lfinalcourseproject;
+
+/**
+ *
+ * @author John Mark Garcia
+ */
+public class CSS124LFinalCourseProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
